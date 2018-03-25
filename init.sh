d4c54869ff717626a2c16d6066221a4bf2240671
Alicelidel <kelifriday@yandex.ru>
2018-03-25 22:54:42 +0300

    lol

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn
+sudo /etc/init.d/gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:hello_func &
