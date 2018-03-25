529265ce2ee0bb9f9e4f82381b57ab62593b7fa1
Alicelidel <kelifriday@yandex.ru>
2018-03-25 23:17:05 +0300

    oops

--- etc/hello.py
+++ etc/hello.py
@@ -1,10 +1,3 @@
-def application(env, start_response):
-    body = [p for p in env['QUERY_STRING'].split('&')]
-    body = '\n'.join(body)
-    status = '200 OK'
-    response_headers = [('Content-Type', 'text/plain'),
-                        ('Content-Length', str(len(body)))
-    ]
-
-    start_response(status, response_headers)
-    return [ body ]
+bind = '0.0.0.0:8080'
+logfile = '../gunicorn.log'
+workers = 1
